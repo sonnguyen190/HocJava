fe242e9dfbafbdc2a55ba4c0ffd5d1763b9fe641
SonNguyen <sonkute190@gmail.com>
2022-03-15 20:14:15 +0700

    ashdjklasd

--- src/main/mang.java
+++ src/main/mang.java
@@ -4,8 +4,10 @@ public class mang {
 //git checkout -b staging qa-->tạo 1 branch mới tên là staging dựa trên qa
 //git branch --> kiểm tra xem mình đang đứng trên branch nào(màu xanh)và liệt kê ra
 //git checkout qa --> chuyển qua branch qa hoặc ngược lại
+//git clone link->lấy source code từ github về local
+//git pull origin-> lấy những thay đổi mới nhất về local
 	public static void main(String[] args) {
-		
+		System.out.println("sdkjfhjkalsdghaslkjd hjlksdhfgkj 234782372735092380");
 
 	}
 
