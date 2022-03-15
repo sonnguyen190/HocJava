266ee098ecec7527c541f95b0506e804b7d99f21
SonNguyen <sonkute190@gmail.com>
2022-03-15 19:33:12 +0700

    lession 7

---
+++ src/main/mang.java
@@ -0,0 +1,12 @@
+package main;
+
+public class mang {
+//git checkout -b staging qa-->tạo 1 branch mới tên là staging dựa trên qa
+//git branch --> kiểm tra xem mình đang đứng trên branch nào(màu xanh)và liệt kê ra
+//git checkout qa --> chuyển qua branch qa hoặc ngược lại
+	public static void main(String[] args) {
+		
+
+	}
+
+}
