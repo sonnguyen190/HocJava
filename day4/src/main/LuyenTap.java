a72c5e0f1202a73fe42c3deae1898d78264bd225
SonNguyen <sonkute190@gmail.com>
2022-03-15 11:36:20 +0700

    còn 3 bàiđể làm sau

---
+++ day4/src/main/LuyenTap.java
@@ -0,0 +1,49 @@
+package main;
+
+import java.util.Scanner;
+
+public class LuyenTap {
+
+	public static void main(String[] args) {
+//		nhapHoVaTen();
+		//tinhTongN();
+
+	}
+//	public static void nhapHoVaTen() {
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("moi nhap ho va ten: ");
+//		String name=scanner.next();
+//		System.out.println("moi nhap diem toan: ");
+//		int toan=scanner.nextInt();
+//		System.out.println("moi nhap diem van");
+//		int van = scanner.nextInt();
+//		int diemTrungBinh=(toan+van)/2;
+//		System.out.println("ten cua ban la: "+name);
+//		System.out.println("diem trung binh la: "+diemTrungBinh);
+//	}
+	
+//	public static void tinhTongN() {
+//		int tong=0;
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("moi nhap N: ");
+//		int n = scanner.nextInt();
+//		for(int i =1;i<=n;i++) {
+//			tong+=i;
+//		}
+//		
+//		System.out.println("tong la: "+ tong);
+//	}
+	
+	public static void tinhTongS() {
+		int luyThua = 1;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("moi nhap X: ");
+		int x = scanner.nextInt();
+		System.out.println("moi nhap n so mu: ");
+		int n = scanner.nextInt();
+		for(int i = 1;i<n;i++) {
+			
+		}
+	}
+
+}
